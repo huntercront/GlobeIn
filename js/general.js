d7e7036815c80b28122a608c051f130187e5f6c8
huntercront <38252870+huntercront@users.noreply.github.com>
2020-02-10 18:07:50 +0300

    add slider

--- js/general.js
+++ js/general.js
@@ -90,5 +90,29 @@ $(window).scroll(function() {
 	}
 });
 
- 
+$('.box-slider').slick({
+	infinite: true,
+	slidesToShow: 3,
+	arrows: true,
+	slidesToScroll: 1,
+	dots: false,
+	responsive: [
+	{
+		breakpoint: 1100,
+		settings: {
+			slidesToShow: 3,
+			slidesToScroll: 1,
+		}
+	},
+	{
+		breakpoint: 768,
+		settings: {
+			slidesToShow: 1,
+			arrows: true,
+			infinite: true,
+			slidesToScroll: 1
+		}
+	}
+	]
+}); 
 
